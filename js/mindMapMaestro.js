be6c2f4d5e3da63cc0226035485285d5511c7952
Jeffrey Home Laptop <jmcda001@github.com>
2016-09-09 15:16:13 -0700

    Adding resize capabilities

--- js/mindMapMaestro.js
+++ js/mindMapMaestro.js
@@ -7,8 +7,11 @@ function MindMap(mindMapDiv,name) {
 
     this.container = mindMapDiv;
     this.data = { nodes: this.ideas,edges: this.connections };
-    this.options = {};
+    this.options = {interaction:{hover:true}};
     this.network = new vis.Network(this.container,this.data,this.options);
+
+    this.network.on('click',function(params) { });
+        
 }
 
 MindMap.prototype.addIdea = function(label) {
@@ -29,25 +32,32 @@ MindMap.prototype.addConnection = function(sourceIdea,sinkIdea) {
 }
 
 MindMap.prototype.addCluster = function(source,sinks) {
-    var sourceIdea = this.addIdea(source);
+    var sourceIdea = this.findIdea(source);
+    if (!sourceIdea) {
+        sourceIdea = this.addIdea(source);
+    }
     for (var sink in sinks) {
         var newSink = this.addIdea(sinks[sink]);
+        newSink.group = source;
         this.addConnection(sourceIdea,newSink);
     }
     return sourceIdea;
 }
 
 MindMap.prototype.findIdea = function(key) {
-    debugger;
     for (var idea in this.ideas._data) {
         if (idea.label == key) { return idea; }
     }
     return null;
 }
 
-$(window).load(function(){
+$(window).on('resize',function() {
+});
+
+$(window).on('load',function(){
 	var mindMap = new MindMap($('#mindMapDiv')[0],'CCE');
 	var ie = mindMap.addIdea('Inland Empire Entrepreneurship');
-	var colleges = mindMap.addCluster('Colleges',['UCR','CBU','La Sierra','CSUSB']);
-	mindMap.addConnection(ie,colleges);
+    var collegeList = ['UCR','CBU','La Sierra','CSUSB'];
+	mindMap.addCluster('Colleges',collegeList);
+	mindMap.addConnection(ie,'Colleges');
 });
